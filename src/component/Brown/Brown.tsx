d52c4fc253e58ccd4b9f9684769286c0a1d09f3c
Mootassam <mootassame@gmail.com>
2023-09-02 20:53:35 +0400

    add the brown Template

--- src/component/Brown/Brown.tsx
+++ src/component/Brown/Brown.tsx
@@ -4,7 +4,18 @@ function Brown(props) {
   return (
     <div className="app__brown">
       <div className="brown__header">
-        <span>17:19</span>
+        <div className="brownheader__left">
+        
+          <span className="time__brown">17:19</span>
+        </div>
+        <div className="brownheader__right">
+          <div className="brown__battery">
+            <div className="cadre__battery">
+              <div className="level__battery"></div>
+            </div>
+            <div className="samll__border"></div>
+          </div>
+        </div>
       </div>
 
       <div className="brown__content">
@@ -13,33 +24,46 @@ function Brown(props) {
             <div className="brown__table1">
               <span>金融機関</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2 ">
+              <span className="gmo">GMO</span>
+              <span className="text__">あおぞらネット銀行 </span>
+              <span className="number">(0310)</span>
+            </div>
           </div>
           <div className="table">
             <div className="brown__table1">
               <span>支店</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2">
+              <span className="text__">あおぞらネット銀行 </span>
+              <span className="number">(102)</span>
+            </div>
           </div>
           <div className="table">
             <div className="brown__table1">
               <span>口座番号</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2 __center">
+              <span className="text__">（普通） </span>
+              <span className="number">1565113</span>
+            </div>
           </div>
           <div className="table">
             <div className="brown__table1">
               <span>受取人名</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2">
+              <span className="text__">クリーンアツプ° （ト*</span>
+            </div>
           </div>
-
           <div className="table">
             <div className="brown__table11">
               <span>振込先口座の登</span>
               <span>録</span>
             </div>
-            <div className="brown__table21"></div>
+            <div className="brown__table21 __center __p5">
+              <span className="text__">振込先口座として登録する</span>
+            </div>
           </div>
         </div>
         <div className="content__second">
@@ -50,28 +74,74 @@ function Brown(props) {
             <div className="brown__table1">
               <span>受取人名</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2 __end">
+              <span className="brown__amount ">3,000,000</span>
+              <span className="brown__currency">円</span>
+            </div>
           </div>
           <div className="table">
             <div className="brown__table1 __item">
               <span>振込手数料</span>
               <span>（税込）</span>
             </div>
-            <div className="brown__table2 __item2"></div>
+            <div className="brown__table2 __item2 __ends">
+              <div className="__end">
+                <span className="brown__amount">145</span>
+                <span className="brown__currency">円</span>
+              </div>
+              <div className="brown__smalltable">
+                <div className="smallheader">
+                  <span>ポイント利用</span>
+                </div>
+                <div className="headers__large">
+                  <span className="semi__header">利用しない</span>
+                  <span className="small__header">利用可能ポイント合計</span>
+                </div>
+                <div className="semi__amount">
+                  <span className="amount__semi">1,101</span>
+                  <span className="title__amount">ポイント</span>
+                </div>
+
+                <div className="large__cadre">
+                  <div className="cadre__top">
+                    <div className="large__left">
+                      <div>
+                        <div className="bults"></div>{" "}
+                        <span className="cadre__text">楽天ポイント</span>
+                      </div>
+                      <div>
+                        <div className="bults"></div>{" "}
+                        <span className="cadre__text">楽天ポイント</span>
+                      </div>
+                    </div>
+                    <div className="large__right">
+                      <span className="cadre__amount">1,101</span>
+                      <span className="small__text">ポイント</span>
+                    </div>
+                  </div>
+                  <div className="last__items">
+                    <span className="last__amount">0</span>
+                    <span className="last__currency">円</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="table">
             <div className="brown__table1">
-              {" "}
               <span>振込予定日</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2 __center">
+              <span className="brown__date"> 2023/07/05</span>
+            </div>
           </div>
-          <div className="table">
-            <div className="brown__table1">
-              {" "}
+          <div className="table  ">
+            <div className="brown__table1 __lastone">
               <span>管理番号</span>
             </div>
-            <div className="brown__table2"></div>
+            <div className="brown__table2  __lastone">
+              <span className="brown__accountnumber">20230705-18992112</span>
+            </div>
           </div>
         </div>
         <div className="content__third">
